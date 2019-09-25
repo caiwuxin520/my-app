78e462a7383303baf36cea53aa82538dbf877f8f
caiwuxin <351779115@qq.com>
2019-09-25 10:40:57 +0800

    .

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
