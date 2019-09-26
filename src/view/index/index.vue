9d0ecdd5043868034cf5bc369d98d970c7db134b
孙耀 <2252370093@qq.com>
2019-09-26 16:42:48 +0800

    111

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -2,13 +2,31 @@
   <div>
     <!-- 首页头部 -->
     <div class="banner">
-      <p class="title">
-        您最高可借款额度（元）：30万
-      </p>
-      <van-slider v-model="value" @change="onChange" />
+      <p class="title">您最高可借款额度（元）：30万</p>
+      <van-slider v-model="value">
+        
+        <div slot="button" class="custom-button"><span>{{ value }}</span></div>
+      </van-slider>
     </div>
     <!-- 借款期限 -->
-    <div></div>
+    <div class="qx">
+      <div class="qx_title">
+        <p>借款期限</p>
+      </div>
+      <div class="qx_money">
+        <span class="left">每月还款（元）</span>
+        <span>月息（元）</span>
+      </div>
+      <div class="xy">
+        <van-checkbox v-model="checked" checked-color="#fc6f08">
+          同意
+          <span>《借款协议》</span>
+        </van-checkbox>
+      </div>
+      <div class="btn">
+        <button>点击&nbsp;&nbsp;&nbsp;&nbsp;马上借款</button>
+      </div>
+    </div>
 
     <!-- 滚动公告 -->
     <div class="text-container">
@@ -16,6 +34,7 @@
         <p class="text" v-for="(text, index) in arr" :key="index">{{text}}</p>
       </div>
     </div>
+    <div class="bb"></div>
     <!-- 封装底部组件 -->
     <tabbar :actives="0"></tabbar>
   </div>
@@ -37,7 +56,8 @@ export default {
         "不是被郭德纲发现的，也不是一开始就收为徒弟。"
       ],
       number: 0,
-      value: 50
+      value: 50,
+      checked: true
     };
   },
   components: {
@@ -69,7 +89,7 @@ export default {
     },
     // 滑块
     onChange(value) {
-      this.$toast('当前值：' + value);
+      this.$toast("当前值：" + value);
     }
   }
 };
@@ -78,17 +98,86 @@ export default {
 
 
 <style lang='less' scoped>
-.banner{
+.custom-button{
+  background: url('../../assets/img/jinbi.png') 100% 100% no-repeat;
+  background-size: 100% 100%;
+  width: 0.6rem;
+  height: 0.6rem;
+}
+.banner {
+  background: url("../../assets/img/bg.png") 100% 100% no-repeat;
+  background-size: 100% 100%;
   text-align: center;
-  margin-bottom:5rem;
+  // margin-bottom:1rem;
+  height: 4rem;
+  line-height: 2.5rem;
+  padding: 0 1rem;
+  color: #fff;
+}
+.qx {
+  background-color: #fff;
+  .qx_title {
+    line-height: 1rem;
+    padding-left: 0.2rem;
+    color: #848484;
+    font-size: 0.32rem;
+    margin-top: 0.1rem;
+  }
+  .qx_money {
+    display: flex;
+    justify-content: center;
+    // line-height: 1.5rem;
+    border-top: 1px solid #efefed;
+    border-bottom: 1px solid #efefed;
+    padding: 0.4rem 0;
+    color: #bdbdbb;
+    .left {
+      border-right: 1px dashed #eeeeee;
+      margin-right: 1rem;
+      padding-right: 1rem;
+    }
+  }
+  .xy {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 0.5rem;
+    span {
+      color: #fc6f08;
+      font-size: 0.32rem;
+    }
+  }
+  .btn {
+    margin: 0 0.22rem;
+    padding-bottom: 0.4rem;
+    button {
+      background-color: #5789ea;
+      border-radius: 0.4rem;
+      color: #fff;
+      font-size: 0.32rem;
+      height: 0.8rem;
+      line-height: 0.8rem;
+      width: 100%;
+      padding: 0 0.22rem;
+      margin: 0 auto;
+
+      // margin:0 0.22rem;
+    }
+  }
 }
 .text-container {
   // width: 500px;
-  height: 30px;
-  line-height: 30px;
+  margin-top: 0.4rem;
+  margin-bottom: 0.4rem;
+  margin-left: 0.5rem;
+  height: 0.6rem;
+  line-height: 0.6rem;
   // margin: 10px auto;
   // border: 1px solid cornflowerblue;
   overflow: hidden;
+}
+.bb {
+  height: 0.8rem;
+  background-color: #fff;
 }
 .text,
 .text2 {
